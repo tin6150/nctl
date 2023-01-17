c999708326a8e96e2220bbc2d4f898721ca9212e
tin6150 <tin_bofh_202206>
2023-01-17 13:32:56 -0800

    running build from NTCL ROOT dir

--- build_ntcl.sh
+++ build_ntcl.sh
@@ -9,10 +9,10 @@ test -d ${NTCL_ROOT} || mkdir -p ${NTCL_ROOT}
 
 cd ${NTCL_ROOT}
 
-git clone https://gitlab.com/ntcl/ntcl-util.git     
-git clone https://gitlab.com/ntcl/ntcl-data.git     
-git clone https://gitlab.com/ntcl/ntcl-tensor.git     
 git clone https://gitlab.com/ntcl/ntcl-algorithms.git     
+git clone https://gitlab.com/ntcl/ntcl-tensor.git     
+git clone https://gitlab.com/ntcl/ntcl-data.git     
+git clone https://gitlab.com/ntcl/ntcl-util.git     
 git clone https://gitlab.com/ntcl/ntcl-build.git 
 git clone https://gitlab.com/ntcl/ntcl-examples.git 
 
@@ -36,11 +36,12 @@ make test | tee -a make_test.log
 
 
 cd ${NTCL_ROOT}/ntcl-build
-bin/ntcl-build.py -h   # sanity check, that it has all required settings and it can at least load
-mkdir /scratch
-bin/ntcl-build.py -c -b /scratch/ntcl
-# ^^ fails?
+./bin/ntcl-build.py -h   # sanity check, that it has all required settings and it can at least load
+#mkdir /scratch
+#bin/ntcl-build.py -c -b /scratch/ntcl
+# ^^ fails
 
-bin/ntcl-build.py -c -t -s default.cuda
-# ^^ fails?
+cd ${NTCL_ROOT}/
+./ntcl-build/bin/ntcl-build.py -c -t -s default.cuda
+# ^^ work like this?
 
